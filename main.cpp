c52d1423d1ccd4899aede00c700f812bf6cef6e5
Alebardock <Alejandrod.morales@hotmail.com>
2023-05-11 06:50:43 -0300

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,45 @@
+#include <SFML/Graphics.hpp>
+#include "../Celda.h"
+
+int main()
+{  /* CrearRegistroCelda();*/
+    Imagen fondo("fondo2.png");
+
+
+    Nivel nivel1;
+    traerNivel(level1,5,5,"Nivel1.dat");
+    sf::RenderWindow Ventana(sf::VideoMode(800, 600), "SFML window");
+    Ventana.setFramerateLimit(60);
+
+    // Load a sprite to display
+    sf::Texture texture;
+   /* if (!texture.loadFromFile("cb.bmp"))
+        return EXIT_FAILURE;*/
+    sf::Sprite sprite(texture);
+
+	// Start the game loop
+    while (Ventana.isOpen())
+    {
+        // Process events
+        sf::Event event;
+        while (Ventana.pollEvent(event))
+        {
+            // Close window : exit
+            if (event.type == sf::Event::Closed)
+                Ventana.close();
+        }
+
+        // Clear screen
+        Ventana.clear();
+
+        // Draw the sprite
+        Ventana.draw(sprite);
+        Ventana.draw(fondo);
+        Ventana.draw(sprite);
+
+        // Update the window
+        Ventana.display();
+    }
+
+    return EXIT_SUCCESS;
+}
